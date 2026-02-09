e5d5eeb8351f5796f7fc23889516c8a1372d3c63
Matheuslemes <matheuslemesmsl@gmail.com>
2026-02-09 16:38:22 -0300

    ecofy(ecofy): ajustes para o funcionamento do gateway

--- api-gateway/src/main/java/br/com/ecofy/gateway/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/br/com/ecofy/gateway/api_gateway/ApiGatewayApplication.java
@@ -1,9 +1,12 @@
 package br.com.ecofy.gateway.api_gateway;
 
+import br.com.ecofy.gateway.api_gateway.config.DynamicGatewayProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DynamicGatewayProperties.class)
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
--- api-gateway/src/main/java/br/com/ecofy/gateway/api_gateway/config/GatewayPropertiesConfig.java
+++
@@ -1,9 +0,0 @@
-package br.com.ecofy.gateway.api_gateway.config;
-
-import br.com.ecofy.gateway.api_gateway.adapters.out.messaging.GatewayLoggingProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(GatewayLoggingProperties.class)
-public class GatewayPropertiesConfig { }
